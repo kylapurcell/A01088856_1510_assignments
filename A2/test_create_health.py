d20ccaa7834b5854e669334d05357791a2044daf
Kyla <kylapurcell18@gmail.com>
2019-02-15 23:42:02 -0800

    finished unit tests for create_health

--- A2/test_create_health.py
+++ A2/test_create_health.py
@@ -1,7 +1,33 @@
 from unittest import TestCase
 from A2 import dungeonsanddragons
+import random
 
 
-class TestCreate_health(TestCase):
+class TestCreateHealth(TestCase):
     def test_create_health(self):
-        self.fail()
+        random.seed(3)
+        self.assertEqual(4, dungeonsanddragons.create_health('rogue'))
+
+    def test_create_health1(self):
+        random.seed(2)
+        self.assertEqual(1, dungeonsanddragons.create_health('fighter'))
+
+    def test_create_health2(self):
+        random.seed(4)
+        self.assertEqual(4, dungeonsanddragons.create_health('barbarian'))
+
+    def test_create_health4(self):
+        random.seed(3)
+        self.assertEqual(2, dungeonsanddragons.create_health('wizard'))
+
+    def test_create_health4(self):
+        self.assertEqual(int, type(dungeonsanddragons.create_health('bard')))
+
+    def test_create_health5(self):
+        random.seed(8)
+        self.assertIn(3, range(dungeonsanddragons.create_health('fighter')))
+
+
+
+
+
