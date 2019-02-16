960bb07ea7c3f0abfd25d046c97eb6e6f9829b7d
Kyla <kylapurcell18@gmail.com>
2019-02-16 14:01:35 -0800

    finished extra unit tests for create_health, added generate vowel test file

--- A2/test_create_health.py
+++ A2/test_create_health.py
@@ -16,17 +16,43 @@ class TestCreateHealth(TestCase):
         random.seed(4)
         self.assertEqual(4, dungeonsanddragons.create_health('barbarian'))
 
-    def test_create_health4(self):
+    def test_create_health3(self):
         random.seed(3)
         self.assertEqual(2, dungeonsanddragons.create_health('wizard'))
 
     def test_create_health4(self):
         self.assertEqual(int, type(dungeonsanddragons.create_health('bard')))
 
-    def test_create_health5(self):
+    def test_create_health42(self):
         random.seed(8)
         self.assertIn(3, range(dungeonsanddragons.create_health('fighter')))
         random.seed()
+
+    def test_create_health5(self):
+        self.assertLessEqual(1, dungeonsanddragons.create_health('barbarian'))
+
+    def test_create_health6(self):
+        self.assertGreaterEqual(12, dungeonsanddragons.create_health('barbarian'))
+
+    def test_create_health7(self):
+        self.assertLessEqual(1, dungeonsanddragons.create_health('cleric'))
+
+    def test_create_health8(self):
+        self.assertGreaterEqual(8, dungeonsanddragons.create_health('cleric'))
+
+    def test_create_health9(self):
+        self.assertLessEqual(1, dungeonsanddragons.create_health('blood hunter'))
+
+    def test_create_health10(self):
+        self.assertGreaterEqual(10, dungeonsanddragons.create_health('blood hunter'))
+
+    def test_create_health11(self):
+        self.assertLessEqual(1, dungeonsanddragons.create_health('sorcerer'))
+
+    def test_create_health12(self):
+        self.assertGreaterEqual(6, dungeonsanddragons.create_health('sorcerer'))
+
+
 
 
 
---
+++ A2/test_generate_vowel.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestGenerate_vowel(TestCase):
+    def test_generate_vowel(self):
+        self.fail()
