4d2595bcc315c8431befd1f87cce371f98daf3a7
Kyla <kylapurcell18@gmail.com>
2019-03-04 13:01:15 -0800

    imported roll_die from A2 and started generate monster function

---
+++ A3/monster.py
@@ -0,0 +1,36 @@
+import random
+def roll_die(number_of_rolls, number_of_sides):
+        """
+        Simulate the rolling of a die a specified number of times with a specified number of sides.
+
+        Calculates a random total with range dependant on number_of_rolls and number_of_sides
+        PARAM: number_of_rolls: a positive integer
+        PARAM: number_of_sides: a positive integer
+        PRE-CONDITION: number_of_rolls must be an integer > 0
+        PRE-CONDITION: number_of_sides must be an integer > 0
+        POST-CONDITION: returns a random sum of a die rolled a specified number of times with specified number of sides
+        RETURN: a random total as a positive integer
+
+        >>> random.seed(1)
+        >>> roll_die(1, 20)
+        5
+        >>> roll_die(3,6)
+        5
+        >>> random.seed()
+        """
+        range_roll_1 = number_of_rolls        # Min total is the rolls
+        range_roll_2 = number_of_sides*number_of_rolls   # Max total is sides * rolls
+        if number_of_rolls > 0 and number_of_sides > 0:
+            return random.randint(range_roll_1, range_roll_2)   # Generates random total with in (min,max) range
+        else:
+            return 0     # Returns zero if rolls and sides = zero
+
+
+def generate_monster():
+    #monster = {#'Class':monster_class_choice(), 'Health': 5, 'Damage': roll_die(1, 4),'Special': 0 }
+    # class2 = monster_class()
+    # monster[special]= monster_class_perk(class2)
+    pass
+    return monster
+
+
