58896798c1d020c9432e76c9fc30bb0bc55e83c6
Kyla <kylapurcell18@gmail.com>
2019-03-05 12:34:38 -0800

    Added create_character function with place holder values in several keys

--- A3/character.py
+++ A3/character.py
@@ -2,7 +2,7 @@ import random
 
 
 def create_character():
-    #input function
-    character = {'Name': '', 'Location': [0, 0] }
+    character = {'Name': input('What is your name? '), 'Health': 10, 'Damage': 0,
+                 'Dexterity': 0, 'Location': [0, 0], 'Inventory': []}
     return character
 
