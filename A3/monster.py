d3ea05ab9229b9d04407cb5c7e1953ac3e58dd32
Kyla <kylapurcell18@gmail.com>
2019-03-05 13:26:43 -0800

    Imported attack_round from A2 to modify, added beginnings monster information function and monster combat function

--- A3/monster.py
+++ A3/monster.py
@@ -1,4 +1,5 @@
 import random
+import character
 
 
 def roll_die(number_of_rolls, number_of_sides):
@@ -68,6 +69,45 @@ def monster_dexterity(class_type):
     elif class_type == 'Pax':
         return roll_die(3, 8)
 
+
+def monster_about():
+    """
+    Print information about the monsters of this world
+    :return:
+    """
+
+def attack_round(attacker, opponent):
+    """
+    Decide if an attack will be successful.
+
+    Rolls an attack based on attacker's class and if > opponent's dexterity, will return the attack value
+    PARAM: attacker, a dictionary
+    PARAM: opponent, a dictionary
+    PRE-CONDITION: attacker must be a complete character's dictionary
+    PRE-CONDITION: opponent must be a complete character's dictionary
+    POST-CONDITION: returns an attack value if attack is successful and 0 if it is not
+    RETURN: an attack as an integer or 0
+    """
+    attack_roll = roll_die(1, 20)
+    damage = roll_die(1, 4)
+    if attack_roll > opponent['Dexterity']:
+        opponent['Health'] = opponent['Health'] - damage
+        print(str(attacker['Name']) + ' rolled a ' + str(attack_roll) + ' , ' + str(opponent['Name'])
+              + ' has a dexterity of ' + str(opponent['Dexterity']))
+        print('Attack was successfully struck')
+        print(attacker['Name'] + ' attacked with a roll of ' + str(damage))
+        return damage
+    elif attack_roll <= opponent['Dexterity']:
+        print(str(attacker['Name']) + ' rolled a ' + str(attack_roll) + ' , ' + str(opponent['Name'])
+              + ' has a dexterity of ' + str(opponent['Dexterity']))
+        print('Attack did not strike')
+        return 0
+
+def monster_combat():
+    """
+    Engage in combat to the death.
+    :return:
+    """
 
 def generate_monster():
     """
