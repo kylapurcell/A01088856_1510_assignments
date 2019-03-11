8d2c98154c74bb9097c2388aa158e0c1982965c7
Kyla <kylapurcell18@gmail.com>
2019-03-10 20:30:31 -0700

    started unit tests for create coordinates

---
+++ A3/test_create_coordinates.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from A3 import sud
+
+class TestCreateCoordinates(TestCase):
+    def test_create_coordinates(self):
+        self.assertEqual(list, type(sud.create_coordinates()))
+
+    def test_create_coordinates2(self):
+        self.assertEqual(49, len(sud.create_coordinates()))
+
+    
