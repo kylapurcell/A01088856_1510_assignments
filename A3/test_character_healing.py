2e0474ead65cb54bd64d7baf33ce2089bd3c96d8
Kyla <kylapurcell18@gmail.com>
2019-03-11 20:41:26 -0700

    doc_test for character_healing

---
+++ A3/test_character_healing.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+from unittest.mock import patch
+import io
+from A3 import character
+
+
+class TestCharacterHealing(TestCase):
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_character_healing(self, mock_stdout):
+        character_one = {'Name': 'Mew', 'Class': 'Hello Kitty', 'Health': 1,
+                         'Dexterity': 0, 'Location': [0, 2], 'Inventory': [], 'Cursed': False}
+        expected_output = 'Your health has revitalized to 2\n'
+        character.character_healing(character_one)
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+    def test_character_healing2(self):
+        character_one = {'Name': 'Mew', 'Class': 'Hello Kitty', 'Health': 10,
+                         'Dexterity': 0, 'Location': [0, 2], 'Inventory': [], 'Cursed': False}
+        self.assertIsNone(character.character_healing(character_one))
+
+        
+
