366a0448560af1f099a16b858aefda503230045b
Kyla <kylapurcell18@gmail.com>
2019-04-04 10:48:24 -0700

    Added more tests to student class unit testing

--- A4/test_student.py
+++ A4/test_student.py
@@ -8,7 +8,7 @@ class TestStudent(TestCase):
 
     def setUp(self):
         self.test_student = student.Student('Kyla', 'Purcell', 'A01088856', True, [80, 90, 75])
-        self.test_student2 = student.Student('Dahlia', 'Billings', 'A01099976', False, [])
+        self.test_student2 = student.Student('dahlia', 'billings', 'A01099976', False, [])
 
     def test_get_first_name(self):
         expected = "Kyla"
@@ -37,7 +37,7 @@ class TestStudent(TestCase):
 
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_print_student_info(self, mock_stdout):
-        expected = 'Kyla Purcell A01088856 True 80 90 75\n'
+        expected = 'Name: Kyla Purcell Student Number: A01088856 Status: True Grades: 80 90 75\n'
         self.test_student.print_student_info()
         self.assertEqual(mock_stdout.getvalue(), expected)
 
@@ -66,4 +66,18 @@ class TestStudent(TestCase):
     def test_set_student_status(self):
         self.test_student.set_student_status(False)
         self.assertEqual(False, self.test_student.get_status())
+
+    def test_format_first_name(self):
+        student2_name = self.test_student2.format_first_name()
+        self.assertEqual('Dahlia', self.test_student2.get_first_name())
+
+    def test_format_last_name(self):
+        student2_name = self.test_student2.format_last_name()
+        self.assertEqual('Billings', self.test_student2.get_last_name())
+
+    def test_repr_(self):
+        expected = 'Kyla Purcell A01088856 True 80 90 75'
+        self.assertEqual(expected, self.test_student.__repr__())
+
+
 
