cb6048e7684d57e78938393ba0e60269ca700ddd
Kyla <kylapurcell18@gmail.com>
2019-04-10 14:00:57 -0700

    question 7 complete

--- A5/question_7.py
+++ A5/question_7.py
@@ -4,13 +4,16 @@ import re
 def password_validator(password: str)->bool:
     while True:
         password_regex = re.compile(r'[A-Z]+[a-z]+\d+')
+        password_regex2 = re.compile(r'^.{7,}$')
         match_object = password_regex.search(password)
-        if match_object:
+        match_object2 = password_regex2.search(password)
+        if match_object and match_object2:
             return True
         else:
             return False
 
-print(password_validator('Helloooo7'))
+
+print(password_validator('hell00007'))
 
 
 
